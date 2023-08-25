e99222385304f05c140462fde4bad16f7ab4b0a0
burak <burak@BURAK>
2023-08-25 21:09:24 +0300

    Proje dosyası ekle.

---
+++ FkaOdev2/Program.cs
@@ -0,0 +1,49 @@
+﻿public abstract class ATM
+{
+    public abstract void ParaYatırma(int miktar);
+    public abstract void ParaÇekme(int miktar);
+    public abstract void BakiyeSorgulama();
+}
+public class NakitATM : ATM
+{
+    private int bakiye = 1000;
+
+    public override void ParaYatırma(int miktar)
+    {
+        bakiye += miktar;
+        Console.WriteLine("Hesabınıza {0} TL yatırıldı.", miktar);
+    }
+    public override void ParaÇekme(int miktar)
+    {
+        if (bakiye < miktar)
+        {
+            Console.WriteLine("Hesap bakiyesi yetersiz.");
+            return;
+        }
+
+        bakiye -= miktar;
+        Console.WriteLine("Hesabınızdan {0} TL çekildi.", miktar);
+    }
+
+    public override void BakiyeSorgulama()
+    {
+        Console.WriteLine("Hesap bakiyeniz {0} TL'dir.", bakiye);
+    }
+}
+public class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        NakitATM nakitATM = new NakitATM();
+
+
+        nakitATM.ParaYatırma(100);
+
+
+        nakitATM.ParaÇekme(50);
+
+
+        nakitATM.BakiyeSorgulama();
+    }
+}
